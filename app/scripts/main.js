04e8ece5e3525bc2745c729ce8dc2c29989e2c42
Kevin Lanni <therealklanni@gmail.com>
2013-06-18 01:27:15 -0700

    temporary script to change badge level

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+// temporary
+var setLevel = function(lv) {
+	var fillColor = ['#333', '#fece5a', '#ffa630', '#ff7315', '#e40000', '#fd2992', '#eb26cd', '#c124e0', '#9627f4'];
+
+	$('.badge-level').removeClass('lv0 lv1 lv2 lv3 lv4 lv5 lv6 lv7 lv8').addClass('lv'+ lv).text(lv);
+	$('#badge-flag-svg').find('path').css('fill', fillColor[lv]);
+};
