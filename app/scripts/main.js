86d8640e32927c4f1fabfbba015b9d45462e99ac
Kevin Lanni <therealklanni@gmail.com>
2013-06-21 19:53:11 -0700

    added spinner

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,5 +3,22 @@ var setLevel = function(lv) {
 	var fillColor = ['#333', '#fece5a', '#ffa630', '#ff7315', '#e40000', '#fd2992', '#eb26cd', '#c124e0', '#9627f4'];
 
 	$('.badge-level').removeClass('lv0 lv1 lv2 lv3 lv4 lv5 lv6 lv7 lv8').addClass('lv'+ lv).text(lv);
-	$('#badge-flag-svg').find('path').css('fill', fillColor[lv]);
+	$('#badge-flag-svg').find('path').css('fill', fillColor[+lv]);
 };
+
+$('#setLevel').spinner({
+	stop: function(event, ui) {
+		setLevel($(this).val());
+	},
+	spin: function(event, ui) {
+		if (ui.value > 8) {
+			$(this).spinner('value', 0);
+
+			return false;
+		} else if (ui.value < 0) {
+			$(this).spinner('value', 8);
+
+			return false;
+		}
+	}
+});
