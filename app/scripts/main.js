622e88851326ad7c6b9ad91436b756a20d9ac88e
Kevin Lanni <therealklanni@gmail.com>
2013-06-23 21:17:37 -0700

    file input support

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,37 @@
 // temporary
-var setLevel = function(lv) {
+var
+
+setLevel = function(lv) {
 	var fillColor = ['#333', '#fece5a', '#ffa630', '#ff7315', '#e40000', '#fd2992', '#eb26cd', '#c124e0', '#9627f4'];
 
 	$('.badge-level').removeClass('lv0 lv1 lv2 lv3 lv4 lv5 lv6 lv7 lv8').addClass('lv'+ lv).text(lv);
 	$('#badge-flag-svg').find('path').css('fill', fillColor[+lv]);
-};
+},
+
+displayImage = function(file, targetElement) {
+	var reader = new FileReader();
+
+	if (file.type.match('image.*')) {
+		reader.onload = (function() {
+			return function(e) {
+				targetElement.src = e.target.result;
+			}
+		})(file);
+
+		reader.readAsDataURL(file);
+	} else {
+		// TODO warn user of unsupported file type
+		return false;
+	}
+}
+
+;
 
 $('#setLevel').spinner({
-	stop: function(event, ui) {
+	stop: function() {
 		setLevel($(this).val());
 	},
-	spin: function(event, ui) {
+	spin: function(_, ui) {
 		if (ui.value > 8) {
 			$(this).spinner('value', 0);
 
@@ -33,23 +54,22 @@ $('.badge-logo, .badge-qr').on({
 		event.preventDefault();
 	},
 	drop: function(event) {
-		event.stopPropagation();
-		event.preventDefault();
-
 		var reader = new FileReader(),
 			file = event.originalEvent.dataTransfer.files[0];
 
-		if (file.type.match('image.*')) {
-			reader.onload = (function(image) {
-				return function(e) {
-					event.target.src = e.target.result;
-				}
-			})(file);
+		event.stopPropagation();
+		event.preventDefault();
 
-			reader.readAsDataURL(file);
-		} else {
-			// TODO warn user of unsupported file type
-			return false;
-		}
+		displayImage(file, event.target);
 	}
+});
+
+$('#userLogo, #userQr').on('change', function(event) {
+	var target = $('.'+ $(this).data('img'))[0],
+		file = $(this)[0].files[0];
+
+	event.stopPropagation();
+	event.preventDefault();
+
+	displayImage(file, target);
 });
