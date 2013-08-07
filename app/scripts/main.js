d8ecaa8c5211c2e6ff3c34eae46b324c8b8e81e3
Kevin Lanni <therealklanni@gmail.com>
2013-08-06 20:11:25 -0700

    G+ Signin support

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,64 @@
 var
 
-spinnerOptions = {
-	stop: function() {
-		setLevel($(this).val());
-	},
-	spin: function(_, ui) {
-		if (ui.value > 8) {
-			$(this).spinner('value', 0);
+agent = {
+	codename: 'CODENAME',
+	level: 0,
+	enlightened: true
+},
 
-			return false;
-		} else if (ui.value < 0) {
-			$(this).spinner('value', 8);
+// TODO when any value is changed draw the badge
+// TODO set save button to call saveImage
 
-			return false;
+// draw front badge design onto canvas
+drawBadgeFront = function(agent) {
+	var blank, logo, scancode, codename, flag, level,
+		canvas = $('#canvas')[0];
+
+	paper.setup(canvas);
+
+	with (paper) {
+		blank = new Raster((agent.enlightened ? 'enl' : 'res') + '-blank-front', new Point(39, 61));
+		blank.scale(0.419, 0.402);
+
+		logo = new Raster('badge-logo', new Point(view.center._x, 113));
+		logo.size = new Size(250, 250);
+		logo.scale(0.545);
+
+		scancode = new Raster('badge-scancode', new Point(63, 203));
+		scancode.size = new Size(109, 21);
+
+		codename = new PointText(new Point(view.center._x, 38));
+		codename.content = agent.codename;
+		codename.characterStyle = {
+			fontSize: 20,
+			font: 'Iceland',
+			justification: 'center'
+		};
+
+		flag = new Path({
+			segments: [[149,190], [149,214], [184,214], [184,173], [164,173]]
+		});
+
+		flag.fillColor = ['#333', '#fece5a', '#ffa630', '#ff7315', '#e40000', '#fd2992', '#eb26cd', '#c124e0', '#9627f4'][agent.level]
+
+		level = new PointText(new Point(166,204));
+		level.content = agent.level;
+		level.fillColor = (agent.level > 0 && agent.level < 8) ? '#333' : '#ffa630';
+		level.characterStyle = {
+			fontSize: 40,
+			font: 'Iceland',
+			justification: 'center'
 		}
 	}
+},
+
+// download canvas as an image
+saveImage = function(filename) {
+	var canvas = $('canvas')[0];
+
+	canvas.toBlob(function(blob) {
+		saveAs(blob, filename);
+	});
 },
 
 // change text and color of level flag
@@ -23,6 +67,8 @@ setLevel = function(lv) {
 
 	$('.badge-level').removeClass('lv0 lv1 lv2 lv3 lv4 lv5 lv6 lv7 lv8').addClass('lv'+ lv).text(lv);
 	$('#badge-flag-svg').find('path').css('fill', fillColor[+lv]);
+
+	return lv;
 },
 
 // handler for displaying dropped or selected images
@@ -32,6 +78,11 @@ displayImage = function(file, targetElement) {
 	if (file.type.match('image.*')) {
 		reader.onload = (function() {
 			return function(e) {
+				targetElement.onload = function() {
+					// update the generated image
+					drawBadgeFront(agent);
+				}
+
 				targetElement.src = e.target.result;
 			}
 		})(file);
@@ -44,6 +95,13 @@ displayImage = function(file, targetElement) {
 }
 
 ;
+
+// save button
+$('button.save').on('click', function(event) {
+	agent.filename = 'Badger - '+ agent.codename +'.png';
+
+	saveImage();
+});
 
 // drag-n-drop event handlers
 $('.badge-logo, .badge-qr').on({
@@ -78,14 +136,45 @@ $('#userLogo, #userQr').on('change', function(event) {
 });
 
 // link inputs with editable elements
-$('#setCodename, #setLink, #setCommname').on('keyup', function() {
-	var $target = $($(this).data('target'));
+$('#setCodename, #setLink, #setCommname').on('keyup change', function() {
+	var $target = $($(this).data('target')),
+		target = $(this).data('target').replace('.badge-', '');
 
-	$target.text($(this).val());
+	agent[target] = $target.text($(this).val().replace('http://','')).text();
+
+	drawBadgeFront(agent);
+});
+
+// link editable elements with inputs
+$('.badge-codename, .badge-link, .badge-community').on('keyup change', function() {
+	var classes = $(this)[0].className,
+		target = classes.slice(classes.indexOf('badge')).split('-')[1],
+		$target = $('#set'+ target[0].toUpperCase() + target.slice(1));
+
+	agent[target] = $target.val($(this).text().replace('http://', '')).val();
+
+	drawBadgeFront(agent);
 });
 
 // level spinner
-$('#setLevel').spinner(spinnerOptions);
+$('#setLevel').spinner({
+	stop: function() {
+		agent.level = setLevel($(this).val());
+
+		drawBadgeFront(agent);
+	},
+	spin: function(_, ui) {
+		if (ui.value > 8) {
+			$(this).spinner('value', 0);
+
+			return false;
+		} else if (ui.value < 0) {
+			$(this).spinner('value', 8);
+
+			return false;
+		}
+	}
+});
 
 $('.badge-flag').on('click', function(event) {
 	$('#setLevel').spinner('stepUp');
@@ -94,13 +183,17 @@ $('.badge-flag').on('click', function(event) {
 
 // swap badge blanks on faction change
 $('#factionRadio').buttonset().on('click', function(event) {
-	if (event.target.id === "factionEnlightened") {
+	agent.enlightened = (event.target.id === 'factionEnlightened');
+
+	if (event.target.id === 'factionEnlightened') {
 		$('.resistance.badge-base').hide();
 		$('.enlightened.badge-base').show();
 	} else {
 		$('.resistance.badge-base').show();
 		$('.enlightened.badge-base').hide();
 	}
+
+	setTimeout(function() { drawBadgeFront(agent); }, 100);
 });
 
 // apply community customizations
